e89ccad72ed3fb3faca2abdf7967a5324b2f878b
srinivasulu0514 <56268088+srinivasulu0514@users.noreply.github.com>
2024-07-28 12:38:27 +0530

    Create variables.tf

---
+++ Modules/variables.tf
@@ -0,0 +1,29 @@
+variable "vm_name" {
+    description = "Name your virtual machine"
+    type = string
+  
+}
+
+variable "vm_size" {
+    description = "Size of your virtual machine"
+    type = string
+  
+}
+
+variable "resorce_group_name" {
+    description = "Name of the resource group"
+    type = string
+  
+}
+
+variable "vnet_name" {
+    description = "Name of the virtual network"
+    type = string
+  
+}
+
+variable "subnet_name" {
+    description = "Name of the subnet"
+    type = string
+  
+}
